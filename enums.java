62d11f5b5b10c9b6562dc5cf8f0864fe5cbe1496
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-28 12:35:00 -0300

    Create enums.java

---
+++ enums.java
@@ -0,0 +1,12 @@
+enum Level {
+  BAIXO,
+  MEDIO,
+  ALTO
+}
+
+public class Main {
+  public static void main (String[] args) {
+    Level minhaVar = Level.MEDIO;
+    System.out.println(minhaVar);
+  }
+}
