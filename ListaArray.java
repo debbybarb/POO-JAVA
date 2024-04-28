3412af6ff6c28626246be38483bbf15c53186e36
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-28 16:20:02 -0300

    Update and rename ListaArrayComAcessoItem.java to ListaArray.java

---
+++ ListaArray.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Ex1 {
+	public static void main (String[] args) {
+		ArrayList<String> carros = new ArrayList<String> ();
+		carros.add("Volvo");
+		carros.add("BMW");
+		carros.add("Ford");
+		carros.add("Mazda");
+		//System.out.println(carros.get(2)); //acessa o item 2 (ford)
+		//carros.set(0, "Opala"); //altera item
+		//carros.remove(0); //remove item
+		//carros.clear(); //remove todos itens do array
+		//System.out.println(carros.size()); //qtd de elementos no array
+		//for (int i=0; i<carros.size(); i++) { //corre pela lista de array acessando cada item
+		//	System.out.println(carros.get(i));
+		//}
+		//for (String i : carros) {
+		//	System.out.println(i);
+		//}
+		
+	}
+}
