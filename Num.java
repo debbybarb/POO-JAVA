6d9be0cdcfb677b766cf50cfba32df02bd75ed24
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-27 21:50:13 -0300

    Create Num.java

---
+++ Num.java
@@ -0,0 +1,17 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 10;
+		y = 20;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		double z;
+		z = 2.3456;
+		System.out.println(String.format("%.2f", z));
+	}
+
+}
