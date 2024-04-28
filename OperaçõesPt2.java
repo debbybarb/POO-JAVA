e5d7d318fc7cb5711ace64e4c30574a96c417699
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-27 21:52:32 -0300

    Create OperaçõesPt2.java

---
+++ OperaçõesPt2.java
@@ -0,0 +1,25 @@
+
+public class Main6 {
+
+	public static void main(String[] args) {
+		double b1, b2, h, area;
+		b1 = 6.0;
+		b2 = 8.0;
+		h = 5.0;
+		area = (b1 + b2) / 2.0 *h;
+		System.out.println(area);
+		
+		int a, b, resultado;
+		a = 5;
+		b = 2;
+		resultado = a /b;
+		System.out.println(resultado);
+		
+		double c;
+		int d;
+		c = 5.0;
+		d = (int) c;
+		System.out.println(d);
+	}
+
+}
