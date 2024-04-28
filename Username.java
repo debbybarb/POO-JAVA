0afc276f2e1f19aeaa2da1b96f61ed78500a6707
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-27 21:47:35 -0300

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main12 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		String userName;
+		
+		System.out.println("Enter username: ");
+		userName = myObj.nextLine();
+		
+		System.out.println("Username is: " + userName);
+	}
+}
