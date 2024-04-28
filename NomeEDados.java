2d691058d9a02019ea7618e01d9dbe2a3e642b33
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-27 21:49:40 -0300

    Create NomeEDados.java

---
+++ NomeEDados.java
@@ -0,0 +1,23 @@
+public class Main2 {
+
+	public static void main(String[] args) {
+		int idade;
+		double salario, altura;
+		char genero;
+		String nome;
+		
+		idade = 23;
+		salario = 00.00;
+		altura = 1.57;
+		genero = 'F';
+		nome = "Débora Barbosa";
+				
+		System.out.println("IDADE = " + idade);
+		System.out.println("SALARIO = " + salario);
+		System.out.println("ALTURA = " + altura);
+		System.out.println("GENERO = " + genero);
+		System.out.println("NOME = " + nome);
+
+	}
+
+}
