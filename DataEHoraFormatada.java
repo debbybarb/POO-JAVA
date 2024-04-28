9887a8d7f3a9af58d298019044109f8f839218e7
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-28 15:27:10 -0300

    Create DataEHoraFormatada.java

---
+++ DataEHoraFormatada.java
@@ -0,0 +1,13 @@
+package meuPacote;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Ex1 {
+	public static void main (String[] args) {
+		LocalDateTime meuObjData = LocalDateTime.now();
+		System.out.println("Antes de formatar: " + meuObjData);
+		DateTimeFormatter meuObjFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		String horaFormatada = meuObjData.format(meuObjFormat);
+		System.out.println("Depois de formatado: " + horaFormatada);
+	}
+}
