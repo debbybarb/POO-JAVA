af1f910c53b6caf3bbd51051f52b47131eb5ee5f
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-28 12:45:36 -0300

    Create enumeracaoEmClasses.java

---
+++ enumeracaoEmClasses.java
@@ -0,0 +1,12 @@
+public class Main {
+  enum Nivel {
+    BAIXO,
+    MEDIO, 
+    ALTO
+  }
+  
+  public static void main (String[] args) {
+    Nivel minhaVar = Nivel.BAIXO;
+    System.out.println(minhaVar);
+  }
+}
