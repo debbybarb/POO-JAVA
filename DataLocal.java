86adfa45e9d4146fcbfa20fb5940ad21db60fc22
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-28 15:08:27 -0300

    Create DataLocal.java

---
+++ DataLocal.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate; //importa a classe LocalDate
+
+public class Ex1 {
+	public static void main(String[] args) {
+		LocalDate meuObj = LocalDate.now(); //cria objeto de data
+		System.out.println(meuObj); //imprime a data atual
+	}
+}
