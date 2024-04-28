16b2701aed23bde1be0425f37e8c1c8550e6f20a
Débora Barbosa <130110668+debbybarb@users.noreply.github.com>
2024-04-27 21:52:10 -0300

    Create OperaçõesPt1.java

---
+++ OperaçõesPt1.java
@@ -0,0 +1,20 @@
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 5;
+		y = 2*x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		int z;
+		double p;
+		z = 5;
+		p = 2 * z;
+		
+		System.out.println(x);
+		System.out.println(String.format("%.2f", p));
+	}
+
+}
